4df9eb6ab509aef00c22e6a5b59ed8ba0d19625a
Purple-Code-sh <purple.code.sh@gmail.com>
2022-07-20 00:21:01 -0500

    implementing new cartesianPlane object to drawing with canvas context and changing some css properties

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth-window.innerWidth*0.05
-canvas.height = window.innerHeight-window.innerHeight*0.05;
+canvas.width = window.innerWidth-window.innerWidth*0.03
+canvas.height = window.innerHeight-window.innerHeight*0.03;
+
+
+const ctx = canvas.getContext("2d");
+
+const cartesianPlane = new CartesianPlane(250,canvas.width,canvas.height);
+
+cartesianPlane.draw(ctx);
