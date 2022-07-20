25b337b8482fd3a6184ddb525cc3a0a1897515a1
Purple-Code-sh <purple.code.sh@gmail.com>
2022-07-20 00:17:08 -0500

    creating file for a cartesianPlane object/class

---
+++ cartesianPlane.js
@@ -0,0 +1,26 @@
+class CartesianPlane {
+    constructor(axes_Length, canvas_width, canvas_height){
+        this.axes_Length = axes_Length;
+        this.canvas_width_center = canvas_width/2;
+        this.canvas_height_center = canvas_height/2;
+    }
+
+    draw(ctx){
+        
+        ctx.strokeStyle = 'purple';
+        ctx.lineWidth = 1;
+        
+        ctx.beginPath();
+
+        //Drawing X axis
+        ctx.moveTo(this.canvas_width_center-this.axes_Length, this.canvas_height_center);
+        ctx.lineTo(this.canvas_width_center+this.axes_Length, this.canvas_height_center);
+
+        //Drawing Y axis
+        ctx.moveTo(this.canvas_width_center, this.canvas_height_center-this.axes_Length);
+        ctx.lineTo(this.canvas_width_center, this.canvas_height_center+this.axes_Length);
+
+        ctx.stroke();
+
+    }
+}
