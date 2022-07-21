fda06ec9acce0abd27d56b29a42279c5a8e78880
Purple-Code-sh <purple.code.sh@gmail.com>
2022-07-21 02:16:20 -0500

    Using the new logarithmicGraph object and giving it parameters in the main script

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ const ctx = canvas.getContext("2d");
 const cartesianPlane = new CartesianPlane(250,canvas.width,canvas.height);
 
 cartesianPlane.draw(ctx);
+
+const logarithmicGraph = new LogarithmicGraph(6,canvas.width,canvas.height)
+
+logarithmicGraph.draw(ctx);
